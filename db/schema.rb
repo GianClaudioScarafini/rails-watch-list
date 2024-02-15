568b74a064e1e05a434a2c8cae1a737a4631857e
GianClaudio Scarafini <g.claudio.scarafini@gmail.com>
2024-02-15 10:28:36 +0000

    added movies and comments and list

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_213020) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_101046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.bigint "list_id", null: false
+    t.text "comment"
     t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "comment"
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
     t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_13_213020) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.string "overview"
+    t.text "overview"
     t.string "poster_url"
     t.float "rating"
     t.datetime "created_at", null: false
